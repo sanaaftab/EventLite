77adb81d0a908d3788ef816bfd86d4cf47f6513b
Guillermo Martin <guillermo.martin@student.manchester.ac.uk>
2020-02-18 11:44:25 +0000

    Created an event in the initial data loader file and deleted the events.json file

--- src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/data/InitialDataLoader.java
@@ -1,5 +1,8 @@
 package uk.ac.man.cs.eventlite.config.data;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
+import uk.ac.man.cs.eventlite.dao.EventRepository;
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 
@@ -32,6 +37,16 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 		}
 
 		// Build and save initial models here.
+		
+		Event event1 = new Event();
+		//event1.setId(1);
+		event1.setDate(LocalDate.of(2020, 02, 25));
+		event1.setTime(LocalTime.of(18, 00));
+		event1.setName("Event 1");
+		event1.setVenue(1);
+		eventService.save(event1);
+//
+//	
 
 	}
 }
--- src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
+++ src/main/java/uk/ac/man/cs/eventlite/dao/EventServiceImpl.java
@@ -27,35 +27,12 @@ public class EventServiceImpl implements EventService {
 	private EventRepository eventRepository;
 
 	@Override
-	public long count() {/*
-		long count = 0;
-		Iterator<Event> i = findAll().iterator();
-
-		for (; i.hasNext(); count++) {
-			i.next();
-		}
-
-		return count; */
+	public long count() {
 		return eventRepository.count();
 	}
 
 	@Override
-	public Iterable<Event> findAll() {/*
-		ArrayList<Event> events = new ArrayList<Event>();
-
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.registerModule(new JavaTimeModule());
-
-			InputStream in = new ClassPathResource(DATA).getInputStream();
-
-			events = mapper.readValue(in, mapper.getTypeFactory().constructCollectionType(List.class, Event.class));
-		} catch (Exception e) {
-			log.error("Exception while reading file '" + DATA + "': " + e);
-			// If we can't read the file, then the event list is empty...
-		}
-
-		return events;*/
+	public Iterable<Event> findAll() {
 		return eventRepository.findAll();
 	}
 	
