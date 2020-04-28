d10d19f13b798a9aa3bfb9c57f39f2f6bfb3432c
Vlad-Andrei Cojocaru <vlad-andrei.cojocaru@student.manchester.ac.uk>
2020-04-28 13:48:16 +0100

    Add all Json links for events and venues.Two need implementation.

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesControllerApi.java
@@ -2,11 +2,11 @@ package uk.ac.man.cs.eventlite.controllers;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -15,6 +15,8 @@ import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -49,19 +51,24 @@ public class VenuesControllerApi {
 
 	private Resource<Venue> venueToResource(Venue venue) {
 		Link selfLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).withSelfRel();
-
-		return new Resource<Venue>(venue, selfLink);
+		Link venuesLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).withRel("venue");
+		//to DO implement the next3events and events links so that the links will go somewhere and give  a result
+		//see the profile one for an example. A new page will be needed probably I am not sure.
+		Link eventsLink = linkTo(VenuesControllerApi.class).slash(venue.getId()).slash("events").withRel("events");
+		Link next3Link = linkTo(VenuesControllerApi.class).slash(venue.getId()).slash("next3events").withRel("next3events");
+		return new Resource<Venue>(venue, selfLink, venuesLink, eventsLink, next3Link);
 	}
 
 	private Resources<Resource<Venue>> venueToResource(Iterable<Venue> venues) {
 		Link selfLink = linkTo(methodOn(VenuesControllerApi.class).getAllVenues()).withSelfRel();
-
+		
+		Link venuesLink = new Link("http://localhost:8080/api/profile/venues").withRel("profile");
 		List<Resource<Venue>> resources = new ArrayList<Resource<Venue>>();
 		for (Venue venue : venues) {
 			resources.add(venueToResource(venue));
 		}
 
-		return new Resources<Resource<Venue>>(resources, selfLink);
+		return new Resources<Resource<Venue>>(resources, selfLink , venuesLink);
 	}
 	
 	//used for deleting a venue 
